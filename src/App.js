b01b63de12f83e0d2ac0caa5c97bb9c30828a30e
Adriano <adriano_alves_2008@hotmail.com>
2022-06-18 19:59:04 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,77 @@
 import './global.css'
 import './styles.css'
 
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup
+  .object({
+    slug: yup.string().required('Esse campo é obrigatório'),
+    title: yup
+      .string()
+      .required('Esse campo é obrigatório')
+      .min(3, 'mínimo de 3 caracteres'),
+    description: yup.string().required('Esse campo é obrigatório'),
+    content: yup.string().required('Esse campo é obrigatório')
+  })
+  .required()
+
 function App() {
+  // O register vai servir para registrar os inputs, para conseguir retirar o valor que a gente colocou dentro do input
+  // O handleSubmit vai sinalizar que estamos enviando uma função após o submit do formulario
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ resolver: yupResolver(schema) })
 
-	return (
-		<div className="app">
-			<form>
-				<h1>Criar publicação</h1>
+  const onSubmitForm = data =>
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        data
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit(onSubmitForm)}>
+        <h1>Criar publicação</h1>
 
-				<div className='field' >
-					<label className='label'>Slug</label>
-					<input type="text" />
-				</div>
+        <div className="field">
+          <label className="label">Slug</label>
+          <input type="text" {...register('slug')} />
+          <p>{errors.slug?.message}</p>
+        </div>
 
-				<div className='field' >
-					<label className='label'>Título</label>
-					<input type="text" />
-				</div>
+        <div className="field">
+          <label className="label">Título</label>
+          <input type="text" {...register('title')} />
+          <p>{errors.title?.message}</p>
+        </div>
 
-				<div className='field' >
-					<label>Descrição</label>
-					<input type="text" />
-				</div>
+        <div className="field">
+          <label>Descrição</label>
+          <input type="text" {...register('description')} />
+          <p>{errors.description?.message}</p>
+        </div>
 
-				<div className='field' >
-					<label>Conteúdo</label>
-					<input type="text" />
-				</div>
+        <div className="field">
+          <label>Conteúdo</label>
+          <input type="text" {...register('content')} />
+          <p>{errors.content?.message}</p>
+        </div>
 
-				<button type='submit'>PUBLICAR</button>
-			</form>
-		</div>
-	);
+        <button type="submit">PUBLICAR</button>
+      </form>
+    </div>
+  )
 }
 
-export default App;
+export default App
